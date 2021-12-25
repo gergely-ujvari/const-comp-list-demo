4f082a6ba257aab9f7bd40511ad2eac33ad0555c
Gergely Ujvari <ujvari@nolmecolindor.com>
2021-12-25 15:14:03 +0100

    Fix formatting

--- meteor_server/imports/api/links.ts
+++ meteor_server/imports/api/links.ts
@@ -1,10 +1,10 @@
 import { Mongo } from 'meteor/mongo';
 
 export interface Link {
-  _id?: string;
-  title: string;
-  url: string;
-  createdAt: Date;
+    _id?: string;
+    title: string;
+    url: string;
+    createdAt: Date;
 }
 
 export const LinksCollection = new Mongo.Collection<Link>('links');
--- meteor_server/server/main.ts
+++ meteor_server/server/main.ts
@@ -2,30 +2,18 @@ import { Meteor } from 'meteor/meteor';
 import { LinksCollection } from '/imports/api/links';
 
 function insertLink(title: string, url: string) {
-  LinksCollection.insert({ title, url, createdAt: new Date() });
+    LinksCollection.insert({ title, url, createdAt: new Date() });
 }
 
 Meteor.startup(() => {
-  // If the Links collection is empty, add some data.
-  if (LinksCollection.find().count() === 0) {
-    insertLink(
-      'Do the Tutorial',
-      'https://www.meteor.com/tutorials/react/creating-an-app'
-    );
+    // If the Links collection is empty, add some data.
+    if (LinksCollection.find().count() === 0) {
+        insertLink('Do the Tutorial', 'https://www.meteor.com/tutorials/react/creating-an-app');
 
-    insertLink(
-      'Follow the Guide',
-      'http://guide.meteor.com'
-    );
+        insertLink('Follow the Guide', 'http://guide.meteor.com');
 
-    insertLink(
-      'Read the Docs',
-      'https://docs.meteor.com'
-    );
+        insertLink('Read the Docs', 'https://docs.meteor.com');
 
-    insertLink(
-      'Discussions',
-      'https://forums.meteor.com'
-    );
-  }
+        insertLink('Discussions', 'https://forums.meteor.com');
+    }
 });
